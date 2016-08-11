b9748a9102b06c7443a0c4206521173c37b0c8d2
Shinji Kakimoto <skakimoto@cuon.co.jp>
2016-08-11 23:52:25 +0900

    model 3

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Contact do
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname:  'Sumner',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+  it "is invalid without lastname and email" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+  it "is invalid without email address"
+  it "is invalid with a duplicate email address"
+  it "returns a contact's full name as a string"
+end
