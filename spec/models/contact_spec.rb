636031c9050d2795b372caf4a2bb8740d0116829
Shinji Kakimoto <skakimoto@cuon.co.jp>
2016-08-13 18:53:17 +0900

    finish 04

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,67 +2,65 @@ require 'rails_helper'
 
 describe Contact do
 
+  it "has a valid factory" do
+    expect(build(:contact)).to be_valid
+  end
+
+  it "has three phone numbers" do
+    expect(create(:contact).phones.count).to eq 3
+  end
+
   it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname:  'Sumner',
-      email: 'tester@example.com')
+    contact = build(:contact)
     expect(contact).to be_valid
   end
 
   it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = build(:contact, firstname: nil )
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   it "is invalid without lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = build(:contact, lastname: nil )
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   it "is invalid without email address" do
-    contact = Contact.new(email: nil)
+    contact =  build(:contact, email: nil )
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: 'Aaron',
-      lastname:  'Sumner',
-      email: 'tester@example.com')
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname:  'Sumner',
-      email: 'tester@example.com')
+    create(:contact, email: 'aaron@example.com' )
+    contact = build(:contact, email: 'aaron@example.com' )
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
   it "returns a contact's full name as a string" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname:  'Sumner',
-      email: 'tester@example.com')
+    contact = build(:contact,
+                     firstname: 'Aaron',
+                     lastname:  'Sumner')
     expect(contact.name).to eq 'Aaron Sumner'
   end
 
   describe "fileter last name by letter" do
     before :each do
-      @smith = Contact.create(
+      @smith = create(:contact,
         firstname: 'John',
         lastname: 'Smith',
         email: 'jsmith@example.com')
-      @jones = Contact.create(
+      @jones = create(:contact,
         firstname: 'Tim',
         lastname: 'Jones',
         email: 'tjones@example.com')
-      @johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com')
+      @johnson = create(:contact,
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjohnson@example.com')
     end
     context "matching letters" do
       it "returns a sorted array of results that match" do
